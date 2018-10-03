7ba87735a2a5b0662349a9428ef01ea216f51667
SAILISONAWANE <saili6794@gmail.com>
2018-10-02 22:06:14 -0400

    Task: testing code 3

--- public/html.php
+++ public/html.php
@@ -8,5 +8,21 @@
 
 class html
 {
-
+    public static function generateTable($records) {
+        $count = 0;
+        foreach ($records as $record) {
+            if($count == 0) {
+                $array = $record->returnArray();
+                $fields = array_keys($array);
+                $values = array_values($array);
+                print_r($fields);
+                print_r($values);
+            } else {
+                $array = $record->returnArray();
+                $values = array_values($array);
+                print_r($values);
+            }
+            $count++;
+        }
+    }
 }
