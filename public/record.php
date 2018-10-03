0edeacfaabc92bea6d74bfe3e52b10c4211d5fa1
SAILISONAWANE <saili6794@gmail.com>
2018-10-02 22:02:03 -0400

    Task: testing code

---
+++ public/record.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: saili
+ * Date: 10/2/18
+ * Time: 9:56 PM
+ */
+
+class record
+{
+    public function __construct(Array $fieldNames = null, $values = null )
+    {
+        $record = array_combine($fieldNames, $values);
+        foreach ($record as $property => $value) {
+            $this->createProperty($property, $value);
+        }
+    }
+    public function returnArray() {
+        $array = (array) $this;
+        return $array;
+    }
+    public function createProperty($name = 'first', $value = 'keith') {
+        $this->{$name} = $value;
+    }
+}
---
+++ public/recordFactory.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: saili
+ * Date: 10/2/18
+ * Time: 9:56 PM
+ */
+
+class recordFactory
+{
+
+}
