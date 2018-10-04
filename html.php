f675f3971208bd9cba3349a82c5fb7a84513cd8c
SAILISONAWANE <saili6794@gmail.com>
2018-10-04 11:22:50 -0400

    Task: HTML trial

--- html.php
+++ html.php
@@ -8,5 +8,158 @@
 
 class html
 {
+    function array2Html($array, $table = true)
+    {
+        $out = '';
+        foreach ($array as $key => $value) {
+            if (is_array($value)) {
+                if (!isset($tableHeader)) {
+                    $tableHeader =
+                        '<th>' .
+                        implode('</th><th>', array_keys($value)) .
+                        '</th>';
+                }
+                array_keys($value);
+                $out .= '<tr>';
+                $out .= array2Html($value, false);
+                $out .= '</tr>';
+            } else {
+                $out .= "<td>$value</td>";
+            }
+        }
+
+        if ($table) {
+            return '<table>' . $tableHeader . $out . '</table>';
+        } else {
+            return $out;
+        }
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 }
